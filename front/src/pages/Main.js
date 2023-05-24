f52020564065143ab5b8e07c673ce5f6cbcd5001
yeeun426 <thsudkcla7@naver.com>
2023-05-25 01:46:09 +0900

    styles : 메인 페이지 ui 구현

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -4,9 +4,30 @@ export default function Main() {
   return (
     <MainStyled>
         <div className = "main-container">
-            <div className = "main-intro">
-                <div>"프론트엔드" 개발자</div>
-                <div>이예은 입니다.</div>
+            <div className = "main-txt-left">
+                <div>안녕하세요👋</div>
+                <div>웹 개발자</div>
+                <div>frontend</div>
+            </div>
+
+            <div className = "main-txt-right">
+              <div>developer</div>
+              <div>이예은입니다.</div>
+            </div>
+            <div className = "main-menu main-project">
+                <div>프로젝트</div>
+            </div>
+
+            <div className = "main-menu main-certificate">
+                <div>자격증</div>
+            </div>
+
+            <div className = "main-menu main-experience">
+                <div>외부 경험</div>
+            </div>
+
+            <div className = "main-myimg">
+              <img src = "images/myimg.png" alt="me"/>
             </div>
         </div>
     </MainStyled>
