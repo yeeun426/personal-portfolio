00d46de99cb365244768a0f15c317fc50245f82b
yeeun426 <thsudkcla7@naver.com>
2023-06-15 21:37:49 +0900

    feat: header

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Intro from './pages/Introduce';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/*' element={<Main/>} />
+        <Route exact path='/intro' element={<Intro/>} />
       </Routes>
     </div>
   );
---
+++ front/src/pages/Introduce.js
@@ -0,0 +1,8 @@
+import { IntroStyled } from "../styles/IntroStyled";
+
+export default function Main() {
+  return (
+    <IntroStyled>
+    </IntroStyled>
+  );
+}
