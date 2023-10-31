b3f39652dc86f95b145078bb03a15071b83ce237
yeeun426 <thsudkcla7@naver.com>
2023-10-31 23:40:10 +0900

    feat : eslint, prettier 적용

---
+++ front/.eslintrc.js
@@ -0,0 +1,35 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended",
+		"standard",
+    "prettier"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
