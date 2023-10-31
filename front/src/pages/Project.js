240229e4beb846dd145fe1a7608b8f318cb21790
yeeun426 <thsudkcla7@naver.com>
2023-10-31 23:38:23 +0900

    feat : eslint, prettier 적용

---
+++ front/src/pages/Project.js
@@ -0,0 +1,11 @@
+import { ProjectStyled } from "../styles/ProjectStyled";
+
+export default function MainIntro() {
+  return (
+    <ProjectStyled> 
+        <div>
+            
+        </div>
+    </ProjectStyled>
+  );
+}
