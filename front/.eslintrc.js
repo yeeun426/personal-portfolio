7a85159f355e6cd0a8efee3385d373564ebee9fe
yeeun426 <thsudkcla7@naver.com>
2023-11-09 23:02:25 +0900

    fix : eslint error

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,36 +1,32 @@
-export default {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: [
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "plugins": ["prettier"],
-    "extends": [
-        "standard-with-typescript",
-        "plugin:prettier/recommended",
-        "eslint:recommended",
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        'react/react-in-jsx-scope': 'off',
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
