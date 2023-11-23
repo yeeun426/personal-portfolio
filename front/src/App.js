819e43311780250a25f94ba980396d2100e7f9b8
yeeun426 <thsudkcla7@naver.com>
2023-11-23 19:00:46 +0900

    error : netlify 배포 오류

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import Intro from './pages/Introduce';
 import Home from './pages/Home/Home';
 
 function App() {
