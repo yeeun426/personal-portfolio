b27b96babc920d1a12514221d49c68ee00fe621f
yeeun426 <thsudkcla7@naver.com>
2023-11-28 18:45:56 +0900

    feat : 메인 글자 추가, 배경 이미지 public 폴더 처리

--- front/src/styles/HomeStyled.js
+++ front/src/styles/HomeStyled.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const HomeStyled = styled.div`
   height: 100vh;
-  background-image: url('https://images.hdqwalls.com/wallpapers/macos-monterey-2g.jpg');
+  background-image: url('images/mac.jpeg');
   background-size: cover;
 
   .home-folder {
     display: flex;
     flex-direction: column;
     align-items: center;
+    z-index: 1;
   }
 
   .home-folder-img {
@@ -27,5 +28,56 @@ export const HomeStyled = styled.div`
     font-size: 25px;
     font-weight: bold;
     margin-top: -10px;
+  }
+
+  .home-txt {
+    font-family: 'Arial';
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 8rem;
+    text-align: left;
+    position: absolute;
+    left: 0;
+    padding: 5rem;
+  }
+
+  span {
+    display: block;
+  }
+
+  span:not(.light) {
+    opacity: 0;
+    animation: flashText 0.8s ease-out alternate infinite;
+  }
+
+  span.light {
+    position: relative;
+    display: inline-block;
+
+    &:before {
+      position: absolute;
+      left: 0;
+      top: 0%;
+      width: 100%;
+      height: 100%;
+      background: #fff;
+      filter: blur(70px);
+      content: '';
+      opacity: 0;
+      animation: flash 0.8s ease-out alternate infinite;
+    }
+  }
+
+  @keyframes flash {
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes flashText {
+    to {
+      opacity: 0.15;
+    }
   }
 `;
