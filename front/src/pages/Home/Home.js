a310a5d741a31d24fd3d32d3a8ea1f0108950c81
yeeun426 <thsudkcla7@naver.com>
2023-11-28 18:45:56 +0900

    feat : 메인 글자 추가, 배경 이미지 public 폴더 처리

--- front/src/pages/Home/Home.js
+++ front/src/pages/Home/Home.js
@@ -9,16 +9,25 @@ export default function Home() {
     <HomeStyled>
       <div className='home-wrapper'>
         {!open ? (
-          <div className='home-folder'>
-            <button onClick={() => setOpen(true)}>
-              <img
-                className='home-folder-img'
-                src='images/folder.png'
-                alt='folder'
-              />
-            </button>
-            <div className='home-folder-name'>이예은</div>
-          </div>
+          <>
+            <div className='home-folder'>
+              <button onClick={() => setOpen(true)}>
+                <img
+                  className='home-folder-img'
+                  src='images/folder.png'
+                  alt='folder'
+                />
+              </button>
+              <div className='home-folder-name'>이예은</div>
+            </div>
+            <div className='home-txt'>
+              <span>Hello!</span>
+              <span>I'm</span>
+              <span class='light'>Yeeun</span>
+              <span>Web developer.</span>
+              <span>welcome</span>
+            </div>
+          </>
         ) : (
           <Popup setOpen={setOpen} />
         )}
