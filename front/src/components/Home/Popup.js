515c38294d551138e7ad5fbf7a76a51d7d2f43d4
yeeun426 <thsudkcla7@naver.com>
2023-12-04 16:11:43 +0900

    feat : popup 변경

--- front/src/components/Home/Popup.js
+++ front/src/components/Home/Popup.js
@@ -1,49 +1,75 @@
-import { AboutMeStyled } from '../../styles/MainStyled';
+import { PopupStyled } from './styled';
 import { Link } from 'react-router-dom';
 
 export default function Popup({ setOpen }) {
   return (
     <div className='mainIntro-wrapper'>
-      <AboutMeStyled>
-        <div className='intro-wrapper-top'>
-          <button onClick={() => setOpen(false)}>
-            <img src='images/minus.png' alt='minimize' />
-          </button>
-          <button className='iwp-img-popup'>
-            <img src='images/popup.png' alt='popup' />
-          </button>
-          <button onClick={() => setOpen(false)}>
-            <img src='images/cancel.png' alt='cancle' />
-          </button>
-        </div>
-        <div className='intro-wrapper'>
-          <div className='intro-wrapper-left'>
-            <div className='iwl-title'>
-              <div>My name is</div>
-              <div className='iwl-title-name'>Ye-eun</div>
-            </div>
-
-            <div className='iwl-txt'>
-              안녕하세요.
-              <br />
-              꾸준하게 빈틈을 채워가며 성장하는 개발자 이예은입니다.
-            </div>
-
-            <div className='iwl-btns'>
-              <Link to='/main'>
-                <button className='iwl-btn-learn'>Learn More 〉</button>
-              </Link>
-              <a href='mailto:thsudkcla7@naver.com'>
-                <button className='iwl-btn-contact'>Contact Me 〉</button>
-              </a>
+      <PopupStyled>
+        <div className='popup-left'>
+          <div className='pl-btn'>
+            <button
+              onClick={() => setOpen(false)}
+              className='popup-close'
+            ></button>
+            <button className='popup-down'></button>
+            <button
+              className='popup-enlarge'
+              onClick={() => setOpen(false)}
+            ></button>
+          </div>
+          <div className='popup-favorite'>
+            <div className='pf-title'>즐겨찾기</div>
+            <div>
+              <button>🏠 home</button>
+              <button>🙋🏼‍♀️ about me</button>
+              <button>🖥️ project</button>
+              <button
+                onClick={() => window.open('https://github.com/yeeun426')}
+              >
+                ✅ github
+              </button>
+              <button
+                onClick={() => window.open('https://yeeeon.tistory.com/')}
+              >
+                📂 tistory
+              </button>
             </div>
           </div>
+        </div>
+        <div className='popup-right'>
+          <div className='pr-top'>
+            <button>
+              <img src='images/popupHeader.png' alt='header' />
+            </button>
+          </div>
+          <div className='popup-content'>
+            <div className='pc-left'>
+              <div className='pcl-title'>
+                <div>My name is</div>
+                <div className='pcl-title-name'>Ye-eun</div>
+              </div>
 
-          <div className='intro-wrapper-right'>
-            <img src='images/myimg.png' alt='yeeun' />
+              <div className='pcl-txt'>
+                안녕하세요.
+                <br />
+                꾸준하게 빈틈을 채워가며 성장하는 개발자 이예은입니다.
+              </div>
+
+              <div className='pcl-btns'>
+                <Link to='/main'>
+                  <button className='pcl-btn-learn'>Learn More 〉</button>
+                </Link>
+                <a href='mailto:thsudkcla7@naver.com'>
+                  <button className='pcl-btn-contact'>Contact Me 〉</button>
+                </a>
+              </div>
+            </div>
+            <div className='pc-right'>
+              <img src='images/myimg.png' alt='yeeun' />
+            </div>
           </div>
         </div>
-      </AboutMeStyled>
+      </PopupStyled>
     </div>
   );
 }
---
+++ front/src/components/Home/styled.js
@@ -0,0 +1,140 @@
+import styled from 'styled-components';
+
+export const PopupStyled = styled.div`
+  display: flex;
+
+  .popup-left {
+    border-radius: 10px 0 0 10px;
+    background: #e9e9e9cf;
+    padding: 20px;
+    box-sizing: border-box;
+    width: 9rem;
+    text-align: left;
+    line-height: 1.7;
+  }
+
+  .pl-btn {
+    display: flex;
+    gap: 8px;
+    margin-bottom: 20px;
+
+    button {
+      border-radius: 50%;
+      width: 14px;
+      height: 14px;
+      border: 1px solid #c1c1c1;
+      box-sizing: border-box;
+    }
+
+    .popup-close {
+      background-color: #ff4c3d;
+    }
+    .popup-down {
+      background-color: #febc2e;
+    }
+    .popup-enlarge {
+      background-color: #2ac840;
+    }
+  }
+
+  .popup-favorite button {
+    padding: 0;
+    font-size: 15px;
+    line-height: 2;
+  }
+
+  .pf-title {
+    font-size: 12px;
+    color: #8b8c8e;
+    font-weight: bold;
+  }
+
+  .popup-right {
+    width: 50rem;
+    margin: auto;
+    border-radius: 0px 10px 10px 0;
+    background: white;
+  }
+
+  .pr-top {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    gap: 10px;
+    border-bottom: 1px solid #d9d9d9;
+    background: #f9f4f9;
+    border-top-right-radius: 10px;
+  }
+
+  .pr-popup {
+    background: white;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    box-sizing: border-box;
+    display: flex;
+    place-content: center;
+    align-items: center;
+
+    img {
+      width: 20px;
+    }
+  }
+
+  .popup-content {
+    padding: 25px 30px;
+    display: flex;
+    place-content: center;
+    align-items: center;
+    gap: 10%;
+    text-align: right;
+  }
+
+  .pcl-title {
+    font-size: 60px;
+  }
+
+  .pcl-title-name {
+    font-weight: bold;
+  }
+
+  .pc-right {
+    img {
+      height: 350px;
+    }
+  }
+
+  .pcl-txt {
+    text-align: left;
+  }
+
+  .pcl-btns {
+    display: flex;
+    align-self: center;
+    gap: 35px;
+  }
+
+  .pc-left {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+
+    button {
+      font-size: 15px;
+      padding: 10px;
+      border-radius: 15px;
+      box-shadow: 2px 2px 4px #00000029;
+    }
+
+    .pcl-btn-contact {
+      border: 3px solid #d483d0;
+      color: #d483d0;
+    }
+
+    .pcl-btn-learn {
+      background: #d483d0;
+      color: white;
+    }
+  }
+`;
