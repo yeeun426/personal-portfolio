68be739a551dd852cb437c2da30537d10509adcd
yeeun426 <thsudkcla7@naver.com>
2023-12-14 17:34:28 +0900

    feat : add project image

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Home from './pages/Home/Home';
+import Project from './pages/Project/Project';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path='/*' element={<Home />} />
         <Route exact path='/main' element={<Main />} />
+        <Route exact path='/project' element={<Project />} />
       </Routes>
     </div>
   );
