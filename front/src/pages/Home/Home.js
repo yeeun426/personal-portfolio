0bdb12bb1fdad1f00fcaa98b43581c9edd6d9385
yeeun426 <thsudkcla7@naver.com>
2024-01-31 18:20:56 +0900

    feat: 타이핑 애니메이션

--- front/src/pages/Home/Home.js
+++ front/src/pages/Home/Home.js
@@ -1,36 +1,65 @@
 import { HomeStyled } from './style';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Popup from '../../components/Home/Popup';
 
 export default function Home() {
   const [open, setOpen] = useState(false);
   const [folder, setFolder] = useState(false);
+  const [name, setName] = useState(false);
+  const [opacity, setOpacity] = useState(false);
+  const [text, setText] = useState('');
+  const content = "Hello\nI'm \n  \n Web developer. \n Welcome";
+  let lineIndex = 0;
+  const animationSpeed = 150;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let txt = content[lineIndex++];
+      setText((prevText) => prevText + (txt === '\n' ? '<br/>' : txt));
+      if (lineIndex >= content.length) {
+        clearInterval(interval);
+      }
+    }, animationSpeed);
+
+    if (content.length !== 0) {
+      setTimeout(function () {
+        setName(true);
+      }, 6500);
+
+      setTimeout(function () {
+        setOpacity(true);
+      }, 7000);
+    }
+
+    return () => clearInterval(interval);
+  }, [content]);
 
   return (
     <HomeStyled>
       <div className='home-wrapper'>
-        <div className='home-txt'>
-          <span className={folder && 'disappear'}>Hello!</span>
-          <span className={folder && 'disappear'}>I'm</span>
-          {!open && (
-            <div className='name-wrapper'>
-              <button
-                className={folder ? 'light moved' : 'light'}
-                onClick={() => setFolder(!folder)}
-              >
-                Yeeun
-              </button>
+        <div
+          className={`home-txt ${opacity ? 'opacity' : ''} ${
+            folder ? 'disappear' : ''
+          }`}
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
+        {name && !open && (
+          <div className='name-wrapper'>
+            <button
+              className={folder ? 'light moved' : 'light'}
+              onClick={() => setFolder(!folder)}
+            >
+              Yeeun
+            </button>
+            {opacity && (
               <img
                 className={folder ? 'home-click disappear' : 'home-click'}
                 src='images/cursor.png'
                 alt='click'
               />
-            </div>
-          )}
-          <span className={folder && 'disappear'}>Web developer.</span>
-          <span className={folder && 'disappear'}>welcome</span>
-        </div>
-
+            )}
+          </div>
+        )}
         <div className={folder ? 'home-folder' : 'home-folder-none'}>
           <button onClick={() => setOpen(true)}>
             <img
@@ -41,7 +70,6 @@ export default function Home() {
           </button>
           <div className='home-folder-name'>이예은</div>
         </div>
-
         {open && <Popup setOpen={setOpen} />}
       </div>
     </HomeStyled>
