f4dd4f2e8ced98c24fee2abcc2e8912866b65274
yeeun426 <thsudkcla7@naver.com>
2024-06-11 23:56:12 +0900

    0611

--- front/src/pages/Portfolio/styled.js
+++ front/src/pages/Portfolio/styled.js
@@ -9,4 +9,28 @@ export const PortfolioStyled = styled.div`
   .header-logo {
     width: 60px;
   }
+
+  .portfolio-title {
+    display: flex;
+    align-items: center;
+    place-content: center;
+    justify-content: space-between;
+    padding: 80px 0;
+    margin-inline-start: auto;
+    margin-inline-end: auto;
+    width: 87.5vw;
+    max-width: 1680px;
+  }
+
+  .title-name {
+    font-size: 80px;
+  }
+
+  .title-detail {
+    font-size: 28px;
+    max-width: 290px;
+    text-align: left;
+    line-height: 1.3;
+    word-break: keep-all;
+  }
 `;
