d2bc075993db2e668e136b8adcd79b5f8b23fecd
yeeun426 <thsudkcla7@naver.com>
2024-12-06 00:27:11 +0900

    feat : TypeIt 적용

---
+++ front/src/components/Start/style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const TestStyled = styled.div`
+  background: black;
+
+  .hi {
+    height: 100vh;
+  }
+
+  .rellax {
+    color: white;
+  }
+`;
+
+export const TypeItStyled = styled.div`
+  height: 100vh;
+
+  .type-it {
+    font-family: 'Arial';
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 8rem;
+    line-height: 1.3;
+  }
+
+  .home-folder {
+    display: flex;
+  }
+
+  .home-folder-none {
+    display: none;
+  }
+`;
