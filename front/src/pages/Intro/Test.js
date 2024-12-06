2afeca7ad49b612a1a17d4ca2e09fd6616b46b77
yeeun426 <thsudkcla7@naver.com>
2024-12-06 20:35:38 +0900

    feat: dropBounce

--- front/src/pages/Intro/Test.js
+++ front/src/pages/Intro/Test.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import TypeItIntro from '../../components/Start/TypeItIntro';
 import { TestStyled } from '../../components/Start/style';
-import Rellax from 'rellax';
 
 function Test() {
-  useEffect(() => {
-    const rellax = new Rellax('.rellax', {
-      horizontal: true,
-      center: true,
-    });
-  }, []);
-
   return (
     <TestStyled>
       <TypeItIntro />
