730c5c3409e7efc4574fd720ac43550f1b1b99c4
yeeun426 <thsudkcla7@naver.com>
2025-01-23 23:04:48 +0900

    feat : css 수정

--- front/src/pages/Ureca/styled.js
+++ front/src/pages/Ureca/styled.js
@@ -5,9 +5,4 @@ export const UrecaStyled = styled.div`
   background-image: url('images/macos.jpg');
   background-size: cover;
   position: relative;
-
-  .pc-right {
-    max-width: 20rem;
-    max-height: 100%;
-  }
 `;
